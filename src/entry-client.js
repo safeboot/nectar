fa55915ef63a281ff33f9e8176cf89ccb8c8e480
Sanjin Brajević <mynamesafeboot@gmail.com>
2024-07-24 00:53:58 +0200

    Removed removed plugin imports

--- src/entry-client.js
+++ src/entry-client.js
@@ -1,10 +1,6 @@
 import './style.css'
 import { createApp } from './main'
 
-import { MotionPlugin } from '@vueuse/motion'
-
 const { app } = createApp()
-
-app.use(MotionPlugin)
 
 app.mount('#app')
